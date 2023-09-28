78e531c5dc0f9664f21440b9da0f694e3c14b4a6
Gadde Chiranjeevi <48180918+gaddechiranjeevi@users.noreply.github.com>
2023-09-28 12:38:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MoviesList from './Movies/MovieList';
+
+function App() {
+  return (
+    <div className="App">
+       <MoviesList/>
+    </div>
+  );
+}
+
+export default App;
